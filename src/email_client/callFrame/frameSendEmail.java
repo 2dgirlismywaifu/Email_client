57fe9be924be92fa53bcde1b3915fec1eaa8cbb5
2dgirlismywaifu <ngothanhlong19943@gmail.com>
2022-06-09 14:49:55 +0700

    up sendEmailForm

---
+++ src/email_client/callFrame/frameSendEmail.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package email_client.callFrame;
+
+import email_client.SendEmail;
+import email_client.global.lookandfeel;
+
+
+/**
+ *
+ * @author notmiyouji
+ */
+public class frameSendEmail {
+    public static void callframe() {
+        lookandfeel.setTheme();
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(() -> {
+            SendEmail frame = new SendEmail();
+            frame.setVisible(true);
+            frame.setTitle("Soạn thư");
+            frame.setResizable(false);
+        });
+    }
+}
