b0a1808a02779df54d4a49272c7ba44a2c55f7d7
2dgirlismywaifu <ngothanhlong19943@gmail.com>
2022-06-12 23:01:55 +0700

    Create FetchContentMail.java
    đang làm phần này

---
+++ src/email_client/utils/FetchContentMail.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package email_client.utils;
+
+/**
+ *
+ * @author notmiyouji
+ */
+public class FetchContentMail {
+    public void ReadMail(int n) {
+        
+    }
+    
+}
