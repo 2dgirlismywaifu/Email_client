a7efd8196391f4b7d00e92e8d6fd59d3625dd825
2dgirlismywaifu <ngothanhlong19943@gmail.com>
2022-06-12 22:59:49 +0700

    Create PropertiesAPI.java
    đây sẽ là phần thêm chỉnh sửa các properties.

---
+++ src/email_client/global/PropertiesAPI.java
@@ -0,0 +1,22 @@
+
+package email_client.global;
+
+public class PropertiesAPI {
+        public String host = "mail.imaps.host";
+        public String port = "mail.imaps.port";
+        public String startTLS = "mail.imaps.starttls.enable";
+
+    public String getHost() {
+        return host;
+    }
+
+    public String getPort() {
+        return port;
+    }
+
+    public String getStartTLS() {
+        return startTLS;
+    }
+        
+    
+}
