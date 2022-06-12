d77958b1e324f315b260c154e361912e88aab465
2dgirlismywaifu <ngothanhlong19943@gmail.com>
2022-06-13 01:41:48 +0700

    Update PropertiesAPI.java

--- src/email_client/global/PropertiesAPI.java
+++ src/email_client/global/PropertiesAPI.java
@@ -2,10 +2,15 @@
 package email_client.global;
 
 public class PropertiesAPI {
-        public String host = "mail.imaps.host";
-        public String port = "mail.imaps.port";
-        public String startTLS = "mail.imaps.starttls.enable";
-        public String trustSSL = "mail.protocol.ssl.trust";
+        public static final String host = "mail.imaps.host";
+        public static final String port = "mail.imaps.port";
+        public static final String startTLS = "mail.imaps.starttls.enable";
+        public static final String trustSSL = "mail.protocol.ssl.trust";
+        public static final String mailProtocol = "mail.store.protocol"; //sử dụng đọc content mail
+
+    public String getMailProtocol() {
+        return mailProtocol;
+    }
 
     public String getTrustSSL() {
         return trustSSL;
