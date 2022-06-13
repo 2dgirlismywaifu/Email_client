67e08a9f245bab807c158cce6c60ed364c94de44
2dgirlismywaifu <ngothanhlong19943@gmail.com>
2022-06-13 11:48:57 +0700

    Create frameAboutSoftware.java

---
+++ src/email_client/callFrame/frameAboutSoftware.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package email_client.callFrame;
+
+import email_client.aboutSoftware;
+import email_client.addAccount;
+import email_client.global.LookandFeel;
+
+/**
+ *
+ * @author notmiyouji
+ */
+public class frameAboutSoftware {
+    public static void callframe() {
+        LookandFeel.setTheme();
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(() -> {
+            aboutSoftware frame = new abstract ();
+            frame.setVisible(true);
+            frame.setTitle("Thông tin phần mềm");
+            frame.setResizable(false);
+        });
+    }
+    
+}
