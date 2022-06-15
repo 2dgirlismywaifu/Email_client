f6edf8ab04f713c775edfee60e763276c690a2e5
2dgirlismywaifu <ngothanhlong19943@gmail.com>
2022-06-16 01:00:04 +0700

    Update LookandFeel.java

--- src/email_client/global/LookandFeel.java
+++ src/email_client/global/LookandFeel.java
@@ -4,7 +4,7 @@
  */
 package email_client.global;
 
-import com.formdev.flatlaf.FlatLightLaf;
+import com.formdev.flatlaf.FlatIntelliJLaf;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
@@ -15,7 +15,7 @@ import javax.swing.UnsupportedLookAndFeelException;
 public class LookandFeel {
     public static void setTheme() {
         try { 
-            UIManager.setLookAndFeel(new FlatLightLaf()); 
+            UIManager.setLookAndFeel(new FlatIntelliJLaf());           
         } catch (UnsupportedLookAndFeelException ex) { 
             ex.printStackTrace(); 
         }
