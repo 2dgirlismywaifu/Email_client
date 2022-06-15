9cd3aa0b4919a66c3cbafcb27097848b7577256b
2dgirlismywaifu <ngothanhlong19943@gmail.com>
2022-06-15 22:34:12 +0700

    sửa lỗi duplicate

--- src/email_client/utils/FetchInbox.java
+++ src/email_client/utils/FetchInbox.java
@@ -3,6 +3,9 @@ package email_client.utils;
 
 import email_client.global.PropertiesAPI;
 import email_client.global.folderMailName;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Properties;
 import javax.mail.FetchProfile;
 import javax.mail.Folder;
@@ -12,23 +15,26 @@ import javax.mail.NoSuchProviderException;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Store;
+import javax.mail.internet.InternetAddress;
+import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 
 
 public class FetchInbox{
     PropertiesAPI propsAPI = new PropertiesAPI();
-    folderMailName foldername = new folderMailName();
     public final DefaultTableModel tableModel = new DefaultTableModel();
-    MailListModel mailListModel = new MailListModel();
-    
+    folderMailName mailName = new folderMailName();
+    MailListModel mailListModel;
+    List<MailList> Data = new  LinkedList<>();
     
     public MailListModel startFetch(String imap, String storeType, String user, String password ) throws NoSuchProviderException, MessagingException {
-             
-              // create properties field
+            Data.clear(); //clear old linked list
+            // create properties field
             Properties properties = new Properties();
             properties.put(propsAPI.getHost(), imap);
             properties.put(propsAPI.getPort(), "993");
             properties.put(propsAPI.getStartTLS(), "true");
+            properties.put(propsAPI.getDebug(), "true");
             properties.put("mail.imaps.partialfetch", "false");
             properties.put("mail.imaps.fetchsize", "2000000");
             
@@ -43,16 +49,38 @@ public class FetchInbox{
                 
             Store store =  emailSession.getStore(storeType);
             store.connect();
-            Folder emailFolder = store.getFolder(foldername.getInboxFolder());
+            Folder emailFolder = store.getFolder(mailName.getInboxFolder());
             emailFolder.open(Folder.READ_WRITE);
-             
             Message[] messages = emailFolder.getMessages();
             FetchProfile fetchProfile = new FetchProfile();
             fetchProfile.add(FetchProfile.Item.ENVELOPE);
             emailFolder.fetch(messages, fetchProfile);
-            mailListModel.setMessages(messages); 
-
-             store.close();         
-        return mailListModel;
+             
+             if (messages.length == 0)
+            {
+                JOptionPane.showMessageDialog(null, "Hộp thư trống", "Thông báo", JOptionPane.INFORMATION_MESSAGE); 
+            }
+            else {
+             for(int i = 0, n = messages.length; i < n; i++) 
+             {     
+                 Message message = messages[i];
+                 String from = "";
+                 InternetAddress[] addresses  =(InternetAddress[]) message.getFrom();
+                          
+                 for(InternetAddress address:addresses)
+                 {
+                     from+=address.getAddress();
+                 }
+                 String[] headers = {"Người Gửi", "Tiêu Đề",  "Thời Gian"};                
+                 Data.add(new MailList(from,message.getSubject(),message.getSentDate().toString()));               
+                 mailListModel = new MailListModel(headers, Data);            
+                 mailListModel.setRowCount(0);
+                                
+                }
+            }
+            
+            emailFolder.close();
+            store.close();
+          return mailListModel;
     }
 }
